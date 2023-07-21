8b304a131816e58bcb4a5f65597b5b327f99a51c
Muharrem Efe Çayırbahçe <96957591+efxlve@users.noreply.github.com>
2023-07-21 12:26:02 +0300

    🚀Project added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 
 import './index.scss';
 
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <UserProvider>
-            <App />
+            <ProductsProvider>
+                <App />
+            </ProductsProvider>
         </UserProvider>
     </BrowserRouter>
 );
