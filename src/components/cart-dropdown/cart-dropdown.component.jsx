b88a9bf6e0a1dfe84fa27f9bc0a80dd733d1f0b5
Muharrem Efe Çayırbahçe <96957591+efxlve@users.noreply.github.com>
2023-07-25 17:46:28 +0300

    🚀Project added

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import './cart-dropdown.styles.scss';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles.jsx';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -14,15 +14,15 @@ const CartDropdown = () => {
     }
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items' >
+        <CartDropdownContainer>
+            <CartItems>
                 {
-                    cartItems.length === 0 ? <span className='empty-message'>Your cart is empty</span> :
+                    cartItems.length === 0 ? <EmptyMessage>Your cart is empty</EmptyMessage> :
                     cartItems.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)
                 }
-            </div>
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     );
 }
 
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,19 @@
-import './directory-item.styles.scss'
+import { useNavigate } from 'react-router-dom';
+import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles.jsx'
 
 const DirectoryItem = ({ category }) => {
-    const { title, imageUrl } = category;
+    const { title, imageUrl, route } = category;
+    const navigate = useNavigate();
+    const onNavigateHandler = () => navigate(route);
+
     return (
-        <div className="directory-item-container">
-            <div className="background-image" style={{
-                backgroundImage: `url(${imageUrl})`
-            }} />
-            <div className="body">
+        <DirectoryItemContainer onClick={onNavigateHandler}>
+            <BackgroundImage imageUrl={imageUrl} />
+            <Body>
                 <h2>{title}</h2>
                 <p>Show Now</p>
-            </div>
-        </div>
+            </Body>
+        </DirectoryItemContainer>
     );
 }
 
