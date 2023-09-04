63507248c5c03cb747db9c3490dc155c5a4ff339
Muharrem Efe Çayırbahçe <96957591+efxlve@users.noreply.github.com>
2023-09-04 12:08:45 +0300

    🚀Project added

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -21,7 +21,7 @@ const ProductCard = ({ product }) => {
             <img src={imageUrl} alt={`${name}`} />
             <Footer>
                 <Name>{name}</Name>
-                <Price>{price}</Price>
+                <Price>${price}</Price>
             </Footer>
             <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>Add to card</Button>
         </ProductCartContainer>
