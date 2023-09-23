0e7f0438f36e151ec2d377a3afdbae5d30450567
Muharrem Efe Çayırbahçe <96957591+efxlve@users.noreply.github.com>
2023-09-24 00:06:42 +0300

    🚀Project added

--- src/components/payment-form/payment-form.styles.tsx
+++ src/components/payment-form/payment-form.styles.tsx
@@ -7,14 +7,27 @@ export const PaymentFormContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
+    @media screen and (max-width:800px) {
+        width: 90vw;
+    }
 `;
 
 export const FormContainer = styled.form`
     height: 100px;
     min-width: 500px;
+
+    @media screen and (max-width:800px) {
+        width: 90vw;
+        min-width: unset;
+    }
 `;
 
 export const PaymentButton = styled(Button)`
     margin-left: auto;
     margin-top: 30px;
+
+    @media screen and (max-width:800px) {
+        margin-top: 20px;
+    }
 `;
--- src/routes/checkout/checkout.styles.tsx
+++ src/routes/checkout/checkout.styles.tsx
@@ -7,6 +7,10 @@ export const CheckoutContainer = styled.div`
     flex-direction: column;
     align-items: center;
     margin: 50px auto 0;
+
+    @media screen and (max-width: 800px) {
+        width: 90vw;
+    }
 `;
 
 export const CheckoutHeader = styled.div`
@@ -15,6 +19,10 @@ export const CheckoutHeader = styled.div`
     display: flex;
     justify-content: space-between;
     border-bottom: 1px solid darkgrey;
+
+    @media screen and (max-width: 800px) {
+        width: 90vw;
+    }
 `;
 
 export const HeaderBlock = styled.div`
@@ -24,10 +32,23 @@ export const HeaderBlock = styled.div`
     &:last-child {
         width: 8%;
     }
+
+    @media screen and (max-width: 800px) {
+        width: 22%;
+        
+        &:last-child {
+            width: 12%;
+        }
+    }
 `;
 
 export const Total = styled.span`
     margin-top: 30px;
     margin-left: auto;
     font-size: 36px;
+
+    @media screen and (max-width: 800px) {
+        margin-top: 10px;
+        font-size: 28px;
+    }
 `;
